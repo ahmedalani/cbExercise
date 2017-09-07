b4501616561fdc00883467710cb4be2cd1cbe603
Ahmed Alani <s3leha@gmail.com>
2017-09-07 18:12:57 +0000

    added the forEach instead of the for loop

--- cbExercise.js
+++ cbExercise.js
@@ -1,10 +1,29 @@
+// // The second argument/parameter is expected to be a function
+// function findWaldo(arr, found) {
+//   for (var i = 0; i < arr.length; i++) {
+//     if (arr[i] === "Waldo") {
+//       found(i);   // execute callback
+//     }
+//   }
+// }
+
+// function actionWhenFound(x) {
+//   console.log("Found him! at index " + x + "!");
+// }
+
+// findWaldo(["Alice", "Bob", "Waldo", "Winston"], actionWhenFound);
+
+
+
+// added the forEach
+
 // The second argument/parameter is expected to be a function
 function findWaldo(arr, found) {
-  for (var i = 0; i < arr.length; i++) {
-    if (arr[i] === "Waldo") {
-      found(i);   // execute callback
+  arr.forEach(function (element, index) {
+    if (element === "Waldo") {
+      found(index);   // execute callback
     }
-  }
+  });
 }
 
 function actionWhenFound(x) {
